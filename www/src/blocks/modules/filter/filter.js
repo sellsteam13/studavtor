a341dd2c3cf31d56fee19ee2e4ae640b87807f56
Yurii Tsimbal <60786111+sellsteam13@users.noreply.github.com>
2020-08-12 22:10:44 +0300

    Commit

--- www/src/blocks/modules/filter/filter.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    if (document.querySelector('.filter-promocode__btn')) {
-        document.querySelector('.filter-promocode__btn').addEventListener('click', (e) => {
-            e.preventDefault();
-            document.querySelector('.filter-promocode').classList.add('is-opened');
-            document.querySelector('.filter-promocode input').focus();
-        });
-    }
-});
--- www/src/js/import/modules.js
+++
@@ -1,8 +0,0 @@
-import "%modules%/header/header";
-import "%modules%/footer/footer";
-import "%modules%/components/components";
-import "%modules%/help/help";
-import "%modules%/reviews/reviews";
-import "%modules%/blog/blog";
-import "%modules%/modals/modals";
-import "%modules%/filter/filter";
